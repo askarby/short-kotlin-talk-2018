f7062b76e8f5c9a37c12c9c461a39ea8b38f8e3d
Anders Skarby <anders@skarby.info>
2018-05-21 15:28:39 +0200

    Implemented first example

---
+++ src/main/java/info/skarby/examples/example1/Example1.java
@@ -0,0 +1,10 @@
+package info.skarby.examples.example1;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        String target = "world";
+        System.out.println(greeting + " " + target);
+    }
+}
---
+++ src/main/kotlin/info/skarby/examples/example1/Example1Result.kt
@@ -0,0 +1,7 @@
+package info.skarby.examples.example1
+
+fun main(args: Array<String>) {
+    val greeting = "Hello"
+    val target = "world"
+    println("$greeting $target")
+}
